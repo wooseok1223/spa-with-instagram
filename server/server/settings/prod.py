961099b2fba052161caf2a0413324a6755f259bb
wooseok1223 <tjdntjr123@gmail.com>
2020-11-09 20:21:08 +0900

    도커를 하기 위한 작업 진행

--- server/server/settings/prod.py
+++ server/server/settings/prod.py
@@ -1,1 +1,27 @@
 from .common import *
+
+DEBUG = os.environ.get("DEBUG") in ["1", "t", "true", "T", "True"]
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
+
+DEFAULT_FILE_STORAGE = 'server.storages.MediaAzureStorage'
+STATICFILES_STORAGE = 'server.storages.StaticAzureStorage'
+
+AZURE_ACCOUNT_NAME = os.environ["AZURE_ACCOUNT_NAME"]
+AZURE_ACCOUNT_KEY = os.environ["AZURE_ACCOUNT_KEY"]
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "level": "ERROR",
+            "class": "logging.StreamHandler",
+        },
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["console"],
+            "level": "ERROR",
+        },
+    },
+}
