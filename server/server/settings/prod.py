1419897ec1b589329959fe8ffbe93a85a767f93c
wooseok1223 <tjdntjr123@gmail.com>
2020-11-10 14:40:28 +0900

    도커 세팅

--- server/server/settings/prod.py
+++ server/server/settings/prod.py
@@ -9,6 +9,16 @@ STATICFILES_STORAGE = 'server.storages.StaticAzureStorage'
 AZURE_ACCOUNT_NAME = os.environ["AZURE_ACCOUNT_NAME"]
 AZURE_ACCOUNT_KEY = os.environ["AZURE_ACCOUNT_KEY"]
 
+DATABASES = {
+    "default": {
+        "ENGINE": os.environ.get("DB_ENGINE", "django.db.backends.postgresql"),
+        "HOST": os.environ["DB_HOST"],
+        "USER": os.environ["DB_USER"],
+        "PASSWORD": os.environ["DB_PASSWORD"],
+        "NAME": os.environ.get("DB_NAME", "postgres"),
+    },
+}
+
 LOGGING = {
     "version": 1,
     "disable_existing_loggers": False,
