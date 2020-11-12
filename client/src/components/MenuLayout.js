92aa652a954da37fae8fd270db2a513aab301b53
wooseok1223 <tjdntjr123@gmail.com>
2020-11-12 11:42:17 +0900

    로그아웃 기능 구현

--- client/src/components/MenuLayout.js
+++ client/src/components/MenuLayout.js
@@ -1,12 +1,34 @@
 import React from 'react'
-import {Avatar, Menu} from "antd";
+import {Avatar, Menu, notification, Button} from "antd";
 import {Link} from "react-router-dom";
-import {useAppContext} from "../store";
+import {deleteToken, useAppContext} from "../store";
+import {useHistory} from 'react-router-dom'
+import {SmileOutlined} from "@ant-design/icons";
 
 const {SubMenu} = Menu;
 
 export default function MenuLayout({author}) {
-    const {store: {isAuthenticated}} = useAppContext()
+    const {store: {isAuthenticated}, dispatch} = useAppContext()
+    const history = useHistory()
+
+    const Logout = () => {
+        async function fn() {
+            try {
+                dispatch(deleteToken(""))
+                notification.open({
+                    message: "로그아웃 성공",
+                    icon: <SmileOutlined style={{color: "#108ee9"}}/>
+                })
+
+                history.push('/')
+            } catch (error) {
+                console.log(error.response)
+            }
+        }
+
+        fn()
+    }
+
     return (
         <>
             {isAuthenticated}
@@ -18,7 +40,7 @@ export default function MenuLayout({author}) {
                     Test
                 </Menu.Item>
                 <SubMenu key="sub1" icon={<Avatar size="large" icon={<img src={author.avatar_url}
-                                                            alt={`${author.username}'s Avatar`}/>}/>}>
+                                                                          alt={`${author.username}'s Avatar`}/>}/>}>
                     <Menu.Item key="1">
                         <Link
                             to={{
@@ -26,8 +48,13 @@ export default function MenuLayout({author}) {
                             }}
                         >
                             <span>Profile</span>
-                        </Link></Menu.Item>
-                    <Menu.Item key="2">Logout</Menu.Item>
+                        </Link>
+                    </Menu.Item>
+                    <Menu.Item key="2" onClick={() => {
+                        Logout()
+                    }}>
+                        <span>Logout</span>
+                    </Menu.Item>
                 </SubMenu>
             </Menu>
         </>
--- client/src/pages/accounts/Profile.js
+++ client/src/pages/accounts/Profile.js
@@ -1,9 +1,69 @@
 import React from 'react'
+import {Button, Card, Col, Form, Input, Row, Typography} from "antd";
+import styled from "styled-components";
+
+const {Text} = Typography;
+
+const Container = styled.div`
+    display:grid;
+    grid-template-columns:repeat(1, 1fr);
+    gap:1rem;
+	width:1024px;
+	max-width:100%;
+	margin:10px auto;
+`;
 
 export default function Profile() {
+    const onFinish = () => {
+
+    }
     return (
-        <div>
-            accounts/profile
-        </div>
+        <Container>
+            <Card align="center" title={<Text strong>Profile</Text>}>
+                <Form
+                    onFinish={onFinish}
+                >
+                    <Row align="center">
+                        <Col span={12}>
+                            <Form.Item
+                                label="Username"
+                                name="username"
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: 'Please input your username!',
+                                    },
+                                    {
+                                        min: 5,
+                                        message: '5글자를 입력해주세요. ',
+                                    }
+                                ]}
+                                hasFeedback
+                                align="center"
+                            >
+                                <Input/>
+                            </Form.Item>
+                        </Col>
+                    </Row>
+                    <Row align="center">
+                        <Col span={12}>
+                            <Form.Item
+                                label="Password"
+                                name="password"
+                                rules={[
+                                    {
+                                        required: true,
+                                        message: 'Please input your password!',
+                                    },
+                                ]}
+                                align="center"
+                            >
+                                <Input.Password/>
+                            </Form.Item>
+                        </Col>
+                    </Row>
+                </Form>
+            </Card>
+        </Container>
     )
 }
