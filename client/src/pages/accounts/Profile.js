ee1afb7ca154224ee606e56e320e406b78ee46f6
wooseok1223 <tjdntjr123@gmail.com>
2020-11-13 22:46:03 +0900

    홈버튼 임시 추가

--- client/src/pages/accounts/Profile.js
+++ client/src/pages/accounts/Profile.js
@@ -1,20 +1,19 @@
 import React, {useEffect, useState} from 'react'
-import {Avatar, Card, Button, Col, Row, Typography} from "antd";
+import {Avatar, Card, Button, Col, Row, Typography, Input} from "antd";
 import styled from "styled-components";
 import {UserOutlined} from '@ant-design/icons';
 import {useAppContext} from "../../store";
 import {useAxios} from "../../api";
 import {Link} from "react-router-dom";
+import AppLayout from "../../components/AppLayout";
+import MenuLayout from "../../components/MenuLayout";
 
 const {Text, Title} = Typography;
 
+
 const Container = styled.div`
-    display:grid;
-    grid-template-columns:repeat(1, 1fr);
-    gap:1rem;
-	width:1024px;
+	width:1000px;
 	max-width:100%;
-	margin:10px auto;
 `;
 
 const NameTitle = styled.div`
@@ -23,6 +22,16 @@ const NameTitle = styled.div`
     gap:1rem;
 	margin:10px auto;
 `;
+
+const Header = styled.div`
+    text-align:center;
+    align:center;
+`
+
+const HeaderTitle = styled.span`
+    text-align:center;
+    font-size:24px;
+`
 
 const apiUrl = "/accounts/profile/"
 
@@ -49,48 +58,60 @@ export default function Profile() {
         }
     }, [originAuthor])
 
-    console.log(author)
 
     return (
-        <Container>
-            <Card align="center">
-                <Row align="center">
-                    <Col span={18} push={6}>
-                        <Card>
-                            <Col span={12} align="left">
-                                <NameTitle>
-                                    <Title level={2}>{author.username}</Title>
-                                    <Link
-                                        to={{
-                                            pathname: "/accounts/profileEdit",
-                                        }}
-                                        style={{marginTop: "10px"}}
-                                    >
+        <div>
+            <Header>
+                <Link
+                    to={{
+                        pathname: "/",
+                    }}
+                >
+                    <HeaderTitle>
+                        LookBook
+                    </HeaderTitle>
+                </Link>
+            </Header>
+            <Container>
+                <Card align="center">
+                    <Row align="center">
+                        <Col span={18} push={6}>
+                            <Card>
+                                <Col span={12} align="left">
+                                    <NameTitle>
+                                        <Title level={2}>{author.username}</Title>
+                                        <Link
+                                            to={{
+                                                pathname: "/accounts/profileEdit",
+                                            }}
+                                            style={{marginTop: "10px"}}
+                                        >
                                             <span>
                                                 프로필 수정으로 이동
                                             </span>
-                                    </Link>
-                                </NameTitle>
-                            </Col>
-                            <Col span={12} align="left">
-                                <Text>{author.email}</Text>
-                            </Col>
-                            <Col span={12} align="left">
-                                <Text>{author.first_name}{author.last_name}</Text>
-                            </Col>
-                            <Col span={12} align="left">
-                                <Text mark>게시물 : 0  팔로워 : 0  팔로우 : 0</Text>
-                            </Col>
-                        </Card>
-                    </Col>
-                    <Col span={6} pull={18}>
-                        <Avatar size={200} icon={<img src={author.avatar_url} alt="user_profile"/>}/>
-                    </Col>
-                </Row>
-                 <Row align="center">
+                                        </Link>
+                                    </NameTitle>
+                                </Col>
+                                <Col span={12} align="left">
+                                    <Text>{author.email}</Text>
+                                </Col>
+                                <Col span={12} align="left">
+                                    <Text>{author.first_name}{author.last_name}</Text>
+                                </Col>
+                                <Col span={12} align="left">
+                                    <Text mark>게시물 : 0 팔로워 : 0 팔로우 : 0</Text>
+                                </Col>
+                            </Card>
+                        </Col>
+                        <Col span={6} pull={18}>
+                            <Avatar size={200} icon={<img src={author.avatar_url} alt="user_profile"/>}/>
+                        </Col>
+                    </Row>
+                    <Row align="center">
 
-                 </Row>
-            </Card>
-        </Container>
+                    </Row>
+                </Card>
+            </Container>
+        </div>
     )
 }
